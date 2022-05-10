c339f40bff11926c3071dcbef638c30e81a63192
RefiningFire <93163433+RefiningFire@users.noreply.github.com>
2022-05-10 14:02:46 -0700

    v0.0.4 removed ideology, added approval bars to pops

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ import decks.starting
 
 class MainScreen(Screen):
     def increase(self):
-        app.stats['pops']['settlers'] += 5
-        app.stats['pops']['teens'] += 4
-        app.stats['pops']['children'] += 3
-        app.stats['pops']['babies'] += 2
+        app.stats['pops']['settlers']['count'] += 5
+        app.stats['pops']['teens']['count'] += 4
+        app.stats['pops']['children']['count'] += 3
+        app.stats['pops']['babies']['count'] += 2
         app.stats['base_modules']['habitation'] += 1
         app.stats['base_modules']['greenhouse'] -= 1
         app.stats['planet_habitability'] += 7
         app.stats['year'] += 1
         app.stats['day'] += 1
-        app.stats['ideologies']['anarchists'] += 1
-        app.stats['ideologies']['authoritarians'] += 2
-        app.stats['ideologies']['communitarians'] += 3
-        app.stats['ideologies']['communists'] += 4
-        app.stats['ideologies']['conservatives'] += 5
+
+        app.stats['pops']['settlers']['approval'] += 5
+        app.stats['pops']['teens']['approval'] += 4
+        app.stats['pops']['children']['approval'] += 3
+        app.stats['pops']['babies']['approval'] += 2
 
 class LoadingScreen(Screen):
     pass
@@ -36,21 +36,22 @@ class TheRememberingOneApp(App):
     def build(self):
         app.stats = {
             'pops':{
-                'babies':0,
-                'children':0,
-                'teens':0,
-                'settlers':1000,
+                'babies':{
+                    'count':0,
+                    'approval':0},
+                'children':{
+                    'count':0,
+                    'approval':0},
+                'teens':{
+                    'count':0,
+                    'approval':0},
+                'settlers':{
+                    'count':1000,
+                    'approval':0},
             },
             'base_modules':{
                 'habitation':100,
                 'greenhouse':10
-            },
-            'ideologies':{
-                'anarchists':20,
-                'authoritarians':30,
-                'communitarians':100,
-                'communists':60,
-                'conservatives':30
             },
             'planet_habitability':0,
             'year':0,
@@ -67,10 +68,15 @@ class TheRememberingOneApp(App):
     def load_buttons(self):
         self.__ids = app.sm.get_screen('main').ids
 
-        self.__ids.babies_count.text = str(app.stats['pops']['babies'])
-        self.__ids.children_count.text = str(app.stats['pops']['children'])
-        self.__ids.teens_count.text = str(app.stats['pops']['teens'])
-        self.__ids.settlers_count.text = str(app.stats['pops']['settlers'])
+        self.__ids.babies_count.text = str(app.stats['pops']['babies']['count'])
+        self.__ids.children_count.text = str(app.stats['pops']['children']['count'])
+        self.__ids.teens_count.text = str(app.stats['pops']['teens']['count'])
+        self.__ids.settlers_count.text = str(app.stats['pops']['settlers']['count'])
+
+        self.__ids.babies_fill.size = (app.stats['pops']['babies']['approval'],self.__ids.babies_fill.parent.height)
+        self.__ids.children_fill.size = (app.stats['pops']['children']['approval'],self.__ids.children_fill.parent.height)
+        self.__ids.teens_fill.size = (app.stats['pops']['teens']['approval'],self.__ids.teens_fill.parent.height)
+        self.__ids.settlers_fill.size = (app.stats['pops']['settlers']['approval'],self.__ids.settlers_fill.parent.height)
 
         self.__ids.planet_habitability.text = str(app.stats['planet_habitability'])
         self.__ids.year.text = str(app.stats['year'])
@@ -78,12 +84,6 @@ class TheRememberingOneApp(App):
 
         self.__ids.habitation.text = str(app.stats['base_modules']['habitation'])
         self.__ids.greenhouse.text = str(app.stats['base_modules']['greenhouse'])
-
-        self.__ids.anarchists_fill.size = (app.stats['ideologies']['anarchists'],self.__ids.anarchists_fill.parent.height)
-        self.__ids.authoritarians_fill.size = (app.stats['ideologies']['authoritarians'],self.__ids.authoritarians_fill.parent.height)
-        self.__ids.communitarians_fill.size = (app.stats['ideologies']['communitarians'],self.__ids.communitarians_fill.parent.height)
-        self.__ids.communists_fill.size = (app.stats['ideologies']['communists'],self.__ids.communists_fill.parent.height)
-        self.__ids.conservatives_fill.size = (app.stats['ideologies']['conservatives'],self.__ids.conservatives_fill.parent.height)
 
 
 if __name__ == '__main__':
