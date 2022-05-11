c8258abf4fca64a03ae134e39b2092ba70a993a5
RefiningFire <93163433+RefiningFire@users.noreply.github.com>
2022-05-11 15:51:59 -0700

    v0.0.6 option selection updates stats.

--- decks/starting.py
+++ decks/starting.py
@@ -6,16 +6,16 @@ deck = {
             'opt1':{
                 'text':'Option 1','new_cards':[''],
                 'cost_type':['settlers_count','babies_count'],'cost_amt':[10,5],
-                'reward':['settlers_approval','babies_approval'],'reward_amt':[20,3]
+                'rwd_type':['settlers_count','babies_approval'],'rwd_amt':[20,1]
             },
             'opt2':{
                 'text':'Option 2','new_cards':[''],
-                'cost_type':['settlers_count'],'cost_amt':[2],
-                'reward':['settlers_approval'],'reward_amt':[2]
+                'cost_type':['settlers_approval'],'cost_amt':[1],
+                'rwd_type':['settlers_count'],'rwd_amt':[2]
             },
             'opt3':{
                 'text':'Option 3','new_cards':[''],
                 'cost_type':['settlers_count'],'cost_amt':[3],
-                'reward':['settlers_count'],'reward_amt':[3]
+                'rwd_type':['settlers_count'],'rwd_amt':[3]
             }}},
 }
--- main.py
+++ main.py
@@ -16,22 +16,22 @@ import decks.starting as starting
 
 class MainScreen(Screen):
     def increase(self):
-        app.stats['pops']['settlers']['count'] += 5
-        app.stats['pops']['teens']['count'] += 4
-        app.stats['pops']['children']['count'] += 3
-        app.stats['pops']['babies']['count'] += 2
+        app.stats['settlers_count'] += 5
+        app.stats['teens_count'] += 4
+        app.stats['children_count'] += 3
+        app.stats['babies_count'] += 2
 
-        app.stats['pops']['settlers']['approval'] += 5
-        app.stats['pops']['teens']['approval'] += 4
-        app.stats['pops']['children']['approval'] += 3
-        app.stats['pops']['babies']['approval'] += 2
+        app.stats['settlers_approval'] += 5
+        app.stats['teens_approval'] += 4
+        app.stats['children_approval'] += 3
+        app.stats['babies_approval'] += 2
 
         app.stats['planet_habitability'] += 7
         app.stats['year'] += 1
         app.stats['day'] += 1
 
-        app.stats['base_modules']['habitation'] += 1
-        app.stats['base_modules']['greenhouse'] -= 1
+        app.stats['habitation_modules'] += 1
+        app.stats['greenhouse_modules'] -= 1
 
     def draw_card(self):
         # Short link to the main screen Float Layout
@@ -47,20 +47,17 @@ class MainScreen(Screen):
 
         # Base of the Card.
         self.current_card = BoxLayout(
-                        orientation='vertical',
-                        spacing=10,
-                        size_hint=(None,None),
-                        width=self.__card_width,
-                        height=self.__card_height,
-                        pos=(self.__parent.width/2-self.__card_width/2,self.__parent.height/2-self.__card_height/2)
-        )
+                            orientation='vertical', spacing=10,
+                            size_hint=(None,None),
+                            width=self.__card_width,height=self.__card_height,
+                            pos=(self.__parent.width/2-self.__card_width/2,self.__parent.height/2-self.__card_height/2))
 
         # Card canvas.
         self.current_card.canvas.add(Color(1,1,1,.5))
         self.current_card.canvas.add(Rectangle(pos=self.current_card.pos,size=(self.__card_width,self.__card_height)))
 
         # Card Title and Text.
-        self.title = Label(text=starting.deck['SD001']['name'])
+        self.title = Label(text=starting.deck['SD001']['name'],font_size=80,size_hint=(1,.3))
         self.text = Label(text=starting.deck['SD001']['text'])
 
         # Add Title and Text to card base.
@@ -70,28 +67,97 @@ class MainScreen(Screen):
         # Iterate over each option in the card.
         for each in range(len(starting.deck['SD001']['options'])):
             # Short link to the current option. (Opt1, opt2, etc.)
-            self.__current_opt = 'opt' + str(each+1)
+            self.__cur_opt = 'opt' + str(each+1)
 
             # The base layout of the layout.
-            self.option_layout = BoxLayout(orientation='horizontal')
+            self.option_layout = BoxLayout(
+                                    orientation='horizontal',
+                                    size_hint_y=None,
+                                    height=100)
 
             # The button that selects the current option.
             self.option_btn = Button(
-                                text=self.__options[self.__current_opt]['text'],
+                                text=self.__options[self.__cur_opt]['text'],
                                 size=self.option_layout.size)
 
             # The cost and reward layouts.
             self.option_cost_layout = BoxLayout(orientation='vertical')
             self.option_reward_layout = BoxLayout(orientation='vertical')
 
+            # Clear the type and amount holders. They will be used when binding the option button.
+            self.cur_opt_cost_type = []
+            self.cur_opt_cost_amt = []
+            self.cur_opt_rwd_type = []
+            self.cur_opt_rwd_amt = []
+
             # Iterate over each resource cost.
-            for i in range(len(self.__options[self.__current_opt]['cost_type'])):
+            for i in range(len(self.__options[self.__cur_opt]['cost_type'])):
+
+                # The current cost type.
+                self.__cost_type = self.__options[self.__cur_opt]['cost_type'][i]
+                # The current cost amount.
+                self.__cost_amt = self.__options[self.__cur_opt]['cost_amt'][i]
+
+                # Save the option type and amt to the button so they can change.
+                self.cur_opt_cost_type.append(self.__cost_type)
+                self.cur_opt_cost_amt.append(self.__cost_amt)
+
                 # Add a label for each cost.
                 self.option_cost = Label(
-                        text=self.__options[self.__current_opt]['cost_type'][i] + ': ' + str(self.__options[self.__current_opt]['cost_amt'][i]))
+                            text=self.__cost_type + ': ' + str(self.__cost_amt))
 
                 # Add the label to the cost layout.
                 self.option_cost_layout.add_widget(self.option_cost)
+
+            # Set types and amounts of cost for each option.
+            if each == 0:
+                self.opt1_cost_types = self.cur_opt_cost_type
+                self.opt1_cost_amt = self.cur_opt_cost_amt
+            elif each == 1:
+                self.opt2_cost_types = self.cur_opt_cost_type
+                self.opt2_cost_amt = self.cur_opt_cost_amt
+            elif each == 2:
+                self.opt3_cost_types = self.cur_opt_cost_type
+                self.opt3_cost_amt = self.cur_opt_cost_amt
+
+            # Iterate over each reward.
+            for i in range(len(self.__options[self.__cur_opt]['rwd_type'])):
+
+                # The current reward type.
+                self.__rwd_type = self.__options[self.__cur_opt]['rwd_type'][i]
+                # The current option amount.
+                self.__rwd_amt = self.__options[self.__cur_opt]['rwd_amt'][i]
+
+                # Save the reward type and amt to the button so they can change.
+                self.cur_opt_rwd_type.append(self.__rwd_type)
+                self.cur_opt_rwd_amt.append(self.__rwd_amt)
+
+                # Add a label for each reward.
+                self.option_reward = Label(
+                            text=self.__rwd_type + ': ' + str(self.__rwd_amt))
+
+                # Add the label to the reward layout.
+                self.option_reward_layout.add_widget(self.option_reward)
+
+            # Set types and amounts of reward for each option.
+            if each == 0:
+                self.opt1_rwd_types = self.cur_opt_rwd_type
+                self.opt1_rwd_amt = self.cur_opt_rwd_amt
+            elif each == 1:
+                self.opt2_rwd_types = self.cur_opt_rwd_type
+                self.opt2_rwd_amt = self.cur_opt_rwd_amt
+            elif each == 2:
+                self.opt3_rwd_types = self.cur_opt_rwd_type
+                self.opt3_rwd_amt = self.cur_opt_rwd_amt
+
+
+            # Bind the cost and rewards to the option button.
+            if each == 0:
+                self.option_btn.bind(on_press=self.opt1_select)
+            elif each == 1:
+                self.option_btn.bind(on_press=self.opt2_select)
+            elif each == 2:
+                self.option_btn.bind(on_press=self.opt3_select)
 
             # Add all the cost layout to the option layout.
             self.option_layout.add_widget(self.option_cost_layout)
@@ -99,20 +165,12 @@ class MainScreen(Screen):
             # Add the option button to the option layout.
             self.option_layout.add_widget(self.option_btn)
 
-            # Iterate over each reward.
-            for i in range(len(self.__options[self.__current_opt]['reward'])):
-                # Add a label for each reward.
-                self.option_reward = Label(
-                            text=self.__options[self.__current_opt]['reward'][i] + ': ' + str(self.__options[self.__current_opt]['reward_amt'][i]))
-
-                # Add the label to the reward layout.
-                self.option_reward_layout.add_widget(self.option_reward)
-
             # Add the reward layout to the option layout.
             self.option_layout.add_widget(self.option_reward_layout)
 
             # Add the option layout to the card base.
             self.current_card.add_widget(self.option_layout)
+
 
         # Once all the layout of the card is done, add it to the Float Layout of the main screen.
         self.__parent.add_widget(self.current_card)
@@ -124,6 +182,25 @@ class MainScreen(Screen):
         # Remove the existing current card from the Float Layout.
         self.__parent.remove_widget(self.current_card)
 
+    # Each options selection iterates through the items in the options button and updates the stats.
+    def opt1_select(self,instance):
+        for item in range(len(self.opt1_cost_types)):
+            app.stats[self.opt1_cost_types[item]] += self.opt1_cost_amt[item]
+        for item in range(len(self.opt1_rwd_types)):
+            app.stats[self.opt1_rwd_types[item]] += self.opt1_rwd_amt[item]
+        app.load_buttons()
+    def opt2_select(self,instance):
+        for item in range(len(self.opt2_cost_types)):
+            app.stats[self.opt2_cost_types[item]] += self.opt2_cost_amt[item]
+        for item in range(len(self.opt2_rwd_types)):
+            app.stats[self.opt2_rwd_types[item]] += self.opt2_rwd_amt[item]
+        app.load_buttons()
+    def opt3_select(self,instance):
+        for item in range(len(self.opt3_cost_types)):
+            app.stats[self.opt3_cost_types[item]] += self.opt3_cost_amt[item]
+        for item in range(len(self.opt3_rwd_types)):
+            app.stats[self.opt3_rwd_types[item]] += self.opt3_rwd_amt[item]
+        app.load_buttons()
 
 
 class LoadingScreen(Screen):
@@ -132,28 +209,19 @@ class LoadingScreen(Screen):
 class TheRememberingOneApp(App):
     def build(self):
         app.stats = {
-            'pops':{
-                'babies':{
-                    'count':0,
-                    'approval':0},
-                'children':{
-                    'count':0,
-                    'approval':0},
-                'teens':{
-                    'count':0,
-                    'approval':0},
-                'settlers':{
-                    'count':1000,
-                    'approval':50},
-            },
-            'base_modules':{
-                'habitation':100,
-                'greenhouse':10
-            },
+            'babies_count':0,
+            'babies_approval':0,
+            'children_count':0,
+            'children_approval':0,
+            'teens_count':0,
+            'teens_approval':0,
+            'settlers_count':1000,
+            'settlers_approval':50,
+            'habitation_modules':100,
+            'greenhouse_modules':10,
             'planet_habitability':0,
             'year':0,
             'day':0
-
         }
 
         self.sm = ScreenManager(transition=SlideTransition())
@@ -165,22 +233,22 @@ class TheRememberingOneApp(App):
     def load_buttons(self):
         self.__ids = app.sm.get_screen('main').ids
 
-        self.__ids.babies_count.text = str(app.stats['pops']['babies']['count'])
-        self.__ids.children_count.text = str(app.stats['pops']['children']['count'])
-        self.__ids.teens_count.text = str(app.stats['pops']['teens']['count'])
-        self.__ids.settlers_count.text = str(app.stats['pops']['settlers']['count'])
+        self.__ids.babies_count.text = str(app.stats['babies_count'])
+        self.__ids.children_count.text = str(app.stats['children_count'])
+        self.__ids.teens_count.text = str(app.stats['teens_count'])
+        self.__ids.settlers_count.text = str(app.stats['settlers_count'])
 
-        self.__ids.babies_fill.size = (app.stats['pops']['babies']['approval'],self.__ids.babies_fill.parent.height)
-        self.__ids.children_fill.size = (app.stats['pops']['children']['approval'],self.__ids.children_fill.parent.height)
-        self.__ids.teens_fill.size = (app.stats['pops']['teens']['approval'],self.__ids.teens_fill.parent.height)
-        self.__ids.settlers_fill.size = (app.stats['pops']['settlers']['approval'],self.__ids.settlers_fill.parent.height)
+        self.__ids.babies_fill.size = (app.stats['babies_approval'],self.__ids.babies_fill.parent.height)
+        self.__ids.children_fill.size = (app.stats['children_approval'],self.__ids.children_fill.parent.height)
+        self.__ids.teens_fill.size = (app.stats['teens_approval'],self.__ids.teens_fill.parent.height)
+        self.__ids.settlers_fill.size = (app.stats['settlers_approval'],self.__ids.settlers_fill.parent.height)
 
         self.__ids.planet_habitability.text = str(app.stats['planet_habitability'])
         self.__ids.year.text = str(app.stats['year'])
         self.__ids.day.text = str(app.stats['day'])
 
-        self.__ids.habitation.text = str(app.stats['base_modules']['habitation'])
-        self.__ids.greenhouse.text = str(app.stats['base_modules']['greenhouse'])
+        self.__ids.habitation.text = str(app.stats['habitation_modules'])
+        self.__ids.greenhouse.text = str(app.stats['greenhouse_modules'])
 
 
 if __name__ == '__main__':
